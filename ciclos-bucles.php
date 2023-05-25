f553db08d9a4f4bbbb0060972f6a62650cb0bb2c
hellisghost <andrademahechafernando@gmail.com>
2023-05-25 14:59:03 -0500

    noooo

---
+++ ciclos-bucles.php
@@ -0,0 +1,94 @@
+<?php
+//ejercicio 1
+
+//Ejercicio de suma y promedio: Escribe un programa que solicite al usuario ingresar una lista de números y luego calcule la suma y el promedio de esos números
+
+
+?>
+
+<?php
+//ejercicio 2
+
+//Ejercicio de números pares: Escribe un programa que muestre todos los números pares del 1 al 100.
+
+for ($i = 0; $i <= 100; $i += 2){
+    echo $i . " ";
+}
+
+?>
+
+<?php
+
+echo "<br>";
+echo "<br>";
+//ejercicio 3
+
+//Ejercicio de factorial: Escribe un programa que calcule el factorial de un número ingresado por el usuario.
+$dato = '4';
+
+$factor = '1';
+for ($var = $dato; $var > 0; $var--){
+    $factor *= $var;
+}
+
+echo "factorial: $factor\n";
+
+?>
+
+<?php
+echo "<br>";
+echo "<br>";
+//ejercicio 4
+
+//Ejercicio de palíndromos: Escribe un programa que verifique si una palabra ingresada por el usuario es un palíndromo (se lee igual de izquierda a derecha y de derecha a izquierda).
+
+?>
+
+<?php
+//ejercicio 5
+
+//Ejercicio de primos: Escribe un programa que determine si un número ingresado por el usuario es primo o no.
+
+
+?>
+
+<?php
+//ejercicio 6
+
+//Ejercicio de reverso: Escribe un programa que invierta un número ingresado por el usuario. Por ejemplo, si el usuario ingresa "12345", el programa debe mostrar "54321".
+
+
+?>
+
+<?php
+//ejercicio 7
+
+//Ejercicio de búsqueda: Escribe un programa que busque un elemento específico en una lista ingresada por el usuario y muestre su posición.
+
+
+?>
+
+<?php
+//ejercicio 8
+
+//Ejercicio de números perfectos: Escribe un programa que encuentre todos los números perfectos en un rango específico. Un número perfecto es aquel cuya suma de sus divisores propios (excluyendo al número mismo) es igual al número.
+
+
+
+
+?>
+
+<?php
+//ejercicio 9
+
+//Ejercicio de Fibonacci: Escribe un programa que genere la secuencia de Fibonacci hasta un número ingresado por el usuario.
+
+
+?>
+
+<?php
+//ejercicio 10
+
+//Ejercicio de ordenamiento: Escribe un programa que ordene una lista de números ingresada por el usuario de menor a mayor utilizando el algoritmo de ordenamiento de burbuja.
+
+?>
